7ad0ddb99c13f9b13e73213314c992655cd32532
Javier <javierin_kl@hotmail.com>
2021-11-22 00:24:33 -0700

    commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import * as THREE from './js/build/three.module.js';
+import {GLTFLoader} from './js/examples/jsm/loaders/GLTFLoader.js'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+scene.add( light );
+
+const loader = new GLTFLoader();
+
+loader.load( './models/scene1.gltf', 
+ ( gltf ) => {
+
+  scene.add( gltf.scene );
+},
+// called while loading is progressing
+( xhr ) => {
+
+  console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+},
+// called when loading has errors
+( error ) => {
+
+  console.log( 'An error happened' );
+
+});
+
+camera.position.z = 5;
+camera.position.y = 6;
+camera.rotation.x = -0.3;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
