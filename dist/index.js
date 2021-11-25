6ebc1d778eee9be5c2f2301580ab34b01e8bfc9b
Javier <javierin_kl@hotmail.com>
2021-11-25 15:52:25 -0700

    webxr

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,31 @@
 import * as THREE from './js/build/three.module.js';
 import {GLTFLoader} from './js/examples/jsm/loaders/GLTFLoader.js'
+import { VRButton } from './js/examples/jsm/webxr/VRButton.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.outputEncoding = THREE.sRGBEncoding;
+
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.xr.enabled = true;
 document.body.appendChild( renderer.domElement );
+
+document.body.appendChild(VRButton.createButton(renderer));
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+window.addEventListener(  'resize', onWindowResize );
+
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -38,10 +56,14 @@ loader.load( './models/scene1.gltf',
 
 camera.position.z = 5;
 camera.position.y = 6;
-camera.rotation.x = -0.3;
+//camera.rotation.x = -0.3;
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+	//requestAnimationFrame( animate );
+	//renderer.render( scene, camera );
+  renderer.setAnimationLoop(()=>{
+    renderer.render( scene, camera)
+
+  });
 }
 animate();
